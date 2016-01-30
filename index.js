e83665d3114b7dab72e4045bf4f8ee2c9c6ff42d
unknown <jeffreyhshapiro@gmail.com>
2016-01-30 17:50:16 -0500

    began constructing prompt in zoo object

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var mysql = require('mysql');
 var password = require('./secret-password.js');
+var prompt = require('prompt');
+prompt.start()
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -16,18 +18,40 @@ connection.connect(function(err) {
     console.log("no error!");
 });
 
-connection.query('SELECT * FROM animals', function(err, rows, fields) {
+var zoo = {
+  welcome: console.log("Welcome to the Zoo and Friends App~!"),
+  menu: function(){
+          console.log("Enter (A): ------> to Add a new animal to the Zoo!")
+          console.log("Enter (U): ------> to Update info on an animal in the Zoo!")
+          console.log("Enter (A): ------> to Add a new animal to the Zoo!")
+          console.log("Enter (A): ------> to Add a new animal to the Zoo!")
+          console.log("Enter (A): ------> to Add a new animal to the Zoo!")
+        },
+  addEntry: function(input_scope){
+    var self = this;
+    var currentScope = input_scope;
+    console.log("To add an animal to the zoo please fill out the following form for us!");
+    prompt.get(['->','name', 'type', 'age'], function (err, result) {
+      var insertQuery = "INSERT INTO animals(name, type, age) VALUES(?,?,?)";
+      connection.query(insertQuery, function(err, rows) {
+      console.log(self.name);
+  });
+    });
+  }
+};
+
+zoo.welcome;
+zoo.menu();
+zoo.addEntry();
+
+connection.end();
+
+/*connection.query('SELECT * FROM animals', function(err, rows, fields) {
   if (err) throw err;
  
   for (var i = 0; i < rows.length; i++) {
     console.log(rows[i].name);
   };
 });
-
-connection.end();
-
-/*var prompt = require('prompt');
-
-prompt.start()
 
 prompt.message = "";*/
